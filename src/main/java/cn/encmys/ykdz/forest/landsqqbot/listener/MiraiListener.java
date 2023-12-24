8df3fe71237d9443db7ffd6c4deeb0800f7aa197
YKDZ <3070799584@qq.com>
2023-12-24 16:53:41 +0800

    初始化仓库

--- src/main/java/cn/encmys/ykdz/forest/landsqqbot/listener/MiraiListener.java
+++ src/main/java/cn/encmys/ykdz/forest/landsqqbot/listener/MiraiListener.java
@@ -1,6 +1,9 @@
 package cn.encmys.ykdz.forest.landsqqbot.listener;
 
 import cn.encmys.ykdz.forest.landsqqbot.util.StringUtils;
+import me.dreamvoid.miraimc.MiraiMCConfig;
+import me.dreamvoid.miraimc.api.MiraiBot;
+import me.dreamvoid.miraimc.api.bot.MiraiFriend;
 import me.dreamvoid.miraimc.bukkit.event.message.passive.MiraiFriendMessageEvent;
 import org.bukkit.event.Listener;
 
@@ -10,10 +13,13 @@ public class MiraiListener implements Listener {
     public void onFriendMessageReceive(MiraiFriendMessageEvent e) {
         long senderId = e.getSenderID();
         String message = e.getMessage();
+        MiraiBot bot = MiraiBot.getBot(e.getBotID());
+        MiraiFriend sender = bot.getFriend(senderId);
 
         if(StringUtils.isQQCommand(message)) {
-            HashMap<String, Object> args = StringUtils.parseQQCommand(message);
-
+            HashMap<StringUtils.QQCommandArgs, Object> args = StringUtils.parseQQCommand(message);
+            sender.sendMessage(args.get(StringUtils.QQCommandArgs.Action).toString());
+            sender.sendMessage("你好");
         }
     }
 }
--- src/main/java/cn/encmys/ykdz/forest/landsqqbot/util/StringUtils.java
+++ src/main/java/cn/encmys/ykdz/forest/landsqqbot/util/StringUtils.java
@@ -1,18 +1,71 @@
 package cn.encmys.ykdz.forest.landsqqbot.util;
 
 import cn.encmys.ykdz.forest.landsqqbot.LandsQQBot;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.YamlConfiguration;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class StringUtils {
-    private final YamlConfiguration messageConfig = LandsQQBot.getMessageConfig();
+    private static final YamlConfiguration messageConfig = LandsQQBot.getMessageConfig();
+    private static final ConfigurationSection argsSection = messageConfig.getConfigurationSection("args");
+
     public static boolean isQQCommand(String message) {
         return message.charAt(0) == '$' && !message.contains("[图片]");
     }
-    public static HashMap<String, Object> parseQQCommand(String message) {
-        HashMap<String, Object> result = new HashMap<>();
+
+    public static HashMap<QQCommandArgs, Object> parseQQCommand(String message) {
+        Map<String, Object> argsMap = argsSection.getValues(false);
+        String regexArgs = String.join("|", messageConfig.getStringList("args-separators"));
+        String regexKeyValue = String.join("|", messageConfig.getStringList("key-value-separators"));
+        String[] args = message.split(regexArgs);
+
+        HashMap<QQCommandArgs, Object> result = new HashMap<>();
+        for(String arg : args) {
+            String[] keyValue = arg.split(regexKeyValue);
+            if(arg.split(regexKeyValue).length != 2) { continue; }
+
+            String key = keyValue[0];
+            String value = keyValue[1];
+
+            if(!argsMap.containsValue(key)) { continue; }
+
+            QQCommandArgs argId = QQCommandArgs.fromString(getKey(argsMap, key));
+
+            if(argId == null) { continue; }
+
+            result.put(argId, value);
+        }
 
         return result;
+    }
+
+    public static <K, V> K getKey(Map<K, V> map, V value) {
+        for (Map.Entry<K, V> entry : map.entrySet()) {
+            if (entry.getValue().equals(value)) {
+                return entry.getKey();
+            }
+        }
+        return null;
+    }
+
+    public enum QQCommandArgs {
+        Land,
+        Player,
+        Area,
+        Nation,
+        Role,
+        Flag,
+        Action;
+
+        public static QQCommandArgs fromString(String name) {
+            for (QQCommandArgs c : QQCommandArgs.values()) {
+                if (c.name().equalsIgnoreCase(name)) {
+                    return c;
+                }
+            }
+            return null;
+        }
     }
 }
