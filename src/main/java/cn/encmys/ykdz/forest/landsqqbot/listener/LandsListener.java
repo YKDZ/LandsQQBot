3cb854c6987f26c76809e8b34b1f1ea711b92dd3
YKDZ <3070799584@qq.com>
2023-12-26 11:06:21 +0800

    初始化仓库

--- src/main/java/cn/encmys/ykdz/forest/landsqqbot/listener/LandsListener.java
+++ src/main/java/cn/encmys/ykdz/forest/landsqqbot/listener/LandsListener.java
@@ -1,5 +1,6 @@
 package cn.encmys.ykdz.forest.landsqqbot.listener;
 
+import me.angeschossen.lands.api.events.LandChatEvent;
 import me.angeschossen.lands.api.events.PlayerLeaveLandEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -7,6 +8,10 @@ import org.bukkit.event.Listener;
 public class LandsListener implements Listener {
     @EventHandler
     public void onPlayerLevelLand(PlayerLeaveLandEvent e) {
+
+    }
+    @EventHandler
+    public void onLandChat(LandChatEvent e) {
 
     }
 }
